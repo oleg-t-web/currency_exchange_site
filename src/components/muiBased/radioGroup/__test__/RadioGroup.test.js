87610a5c4c95b6cdcbb6dc0b1b7c2db5bb566913
oleg_t <ut3uge@ukr.net>
2022-07-02 14:33:31 +0300

    some small tests

---
+++ src/components/muiBased/radioGroup/__test__/RadioGroup.test.js
@@ -0,0 +1,20 @@
+import { render, screen } from '@testing-library/react';
+import { OPERATIONS } from '../../../../CurrencyConstants';
+import userEvent from '@testing-library/user-event';
+import RadioGroup from '../RadioGroup';
+
+test('change operation', () => {
+  render(
+    <RadioGroup
+      currentValue={OPERATIONS.BUY}
+      valuesList={OPERATIONS}
+      handleValueChange={() => {}}
+    />
+  );
+
+  const buyButton = screen.getByRole('radio', { name: OPERATIONS.BUY });
+  const sellButton = screen.getByRole('radio', { name: OPERATIONS.SELL });
+
+  userEvent.click(buyButton, { button: 0 });
+  expect(sellButton.checked).toBe(false);
+});
