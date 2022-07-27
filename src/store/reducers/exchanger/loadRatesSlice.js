5d9a55651b61826f06a92a2d7624dd8fa65349aa
o.tomai <o.tomai@temabit.com>
2022-07-27 18:15:31 +0300

    fixed waitIndicator

--- src/store/reducers/exchanger/loadRatesSlice.js
+++ src/store/reducers/exchanger/loadRatesSlice.js
@@ -1,27 +1,33 @@
+//import { useDispatch } from 'react-redux';
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 import { api as currencyApi } from 'api/currencyApi';
 import prepareRates from 'store/actionCreators/exchanger/helpers/prepareRates';
 
-// import {
-//   hideLoaderIndicarotAction,
-//   setErrorMessageAction,
-//   showLoaderIndicarotAction
-// } from '../loaderIndicatorSlice';
+import {
+  hideLoaderIndicarotAction,
+  setErrorMessageAction,
+  showLoaderIndicarotAction
+} from '../loaderIndicatorSlice';
 
-export const loadCurrencyAction = createAsyncThunk('loadRates/loadCurrencyAction', async () => {
-  // dispatch(showLoaderIndicarotAction());
-  //dispatch(setErrorMessageAction(''));
-  return await currencyApi
-    .getCurrencyRates()
-    .then((rates) => {
-      console.log('Rates> ', rates);
-      return prepareRates(rates);
-    })
-    .catch((err) => {
-      console.log('ERR>', err);
-      //dispatch(setErrorMessageAction(err.message));
-    });
-});
+export const loadCurrencyAction = createAsyncThunk(
+  'loadRates/loadCurrencyAction',
+  async (_, thunkApi) => {
+    const dispatch = thunkApi.dispatch;
+    dispatch(showLoaderIndicarotAction());
+    return currencyApi
+      .getCurrencyRates()
+      .then((rates) => {
+        const preparedRates = prepareRates(rates);
+        dispatch(hideLoaderIndicarotAction());
+        return preparedRates;
+      })
+      .catch((err) => {
+        console.log('ERR>', err);
+        dispatch(setErrorMessageAction(err.message));
+        dispatch(hideLoaderIndicarotAction());
+      });
+  }
+);
 
 const loadRatesSlice = createSlice({
   name: 'loadRates',
@@ -34,12 +40,6 @@ const loadRatesSlice = createSlice({
       state.currentRates = action.payload;
     });
   }
-  // reducers: {
-  // loadCurrencyAction(state, action) {
-  //   console.log('rates payload********> ', action);
-  //   state.currentRates = action.payload;
-  // }
-  // }
 });
 
 export default loadRatesSlice.reducer;
