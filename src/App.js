08647764a9c2576b60c63944cbbf2bffa02a45b1
oleg_t <ut3uge@ukr.net>
2022-07-30 18:51:57 +0300

    small fix

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { init as initExchangerApi } from 'api/currencyApi';
 import TransactionHistoryContextProvider from 'contexts/TransactionHistoryContext';
@@ -13,13 +13,17 @@ import NotFound from './pages/NotFound/NotFound';
 import PAGES from './routes/Links';
 
 function App() {
-  initExchangerApi();
+  useEffect(() => {
+    initExchangerApi();
+  }, []); // todo ask why
+
   const { isLoading } = useLoadIndicator();
   return (
     <div>
       <Router>
         <TransactionHistoryContextProvider>
           <Navbar />
+          {/* todo make separate */}
           {isLoading && <WaitIndicator />}
           <div>
             <Routes>
